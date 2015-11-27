6b2a0d5a06c6b57ff597fcbe628f0d9b225bb436
Pascal Tung <pascal.tung.1@ulaval.ca>
2015-11-26 22:30:00 -0500

    TP3 Correction : Borne

--- BorneTesteur/BorneTesteur.cpp
+++ BorneTesteur/BorneTesteur.cpp
@@ -83,10 +83,19 @@ public:
 /**
  * \brief Test de la methode bool operator==(const Borne& p_borne) const;
  */
-TEST_F(BorneTestValide, opEgal)
+TEST_F(BorneTestValide, opEgalValide)
 {
 	BorneTest borneTest(10, "Nord", "Nom topographique", 10.0, 20.0);
 	ASSERT_TRUE(borneTest == borneTestval);
+}
+
+/**
+ * \brief Test de la methode bool operator==(const Borne& p_borne) const;
+ */
+TEST_F(BorneTestValide, opEgalInvalide)
+{
+	BorneTest borneTest(100, "Nord", "Nom topographique", 10.0, 20.0);
+	ASSERT_FALSE(borneTest == borneTestval);
 }
 
 /**
@@ -133,9 +142,18 @@ TEST_F(BorneTestValide, reqLatitude)
  * \brief Test de la methode
  * void asgNomTopographique(const std::string& p_nomTopographique);
  */
-TEST_F(BorneTestValide, asgNomTopographique)
+TEST_F(BorneTestValide, asgNomTopographiqueValide)
 {
 	ASSERT_EQ("Nom topographique", borneTestval.reqNomTopographique());
 	borneTestval.asgNomTopographique("Nom 2");
 	ASSERT_EQ("Nom 2", borneTestval.reqNomTopographique());
+}
+
+/**
+ * \brief Test de la methode
+ * void asgNomTopographique(const std::string& p_nomTopographique);
+ */
+TEST_F(BorneTestValide, asgNomTopographiqueInvalide)
+{
+	ASSERT_THROW(borneTestval.asgNomTopographique(""), ContratException);
 }
