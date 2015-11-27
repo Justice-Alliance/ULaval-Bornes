8cd95fdac5b3dc0ceb2cbaf44fe99938628237ea
Misuke Nakamura <misuke01@gmail.com>
2015-11-26 20:12:53 -0800

    Iterator is better + remove debug files

--- Borne/Debug/Borne.d
+++
@@ -1,8 +0,0 @@
-Borne.d: ../Borne.cpp ../Borne.h ../ContratException.h \
- ../validationFormat.h
-
-../Borne.h:
-
-../ContratException.h:
-
-../validationFormat.h:
--- Borne/Debug/RegistreBorne.d
+++
@@ -1,8 +0,0 @@
-RegistreBorne.d: ../RegistreBorne.cpp ../RegistreBorne.h \
- ../ContratException.h ../Borne.h
-
-../RegistreBorne.h:
-
-../ContratException.h:
-
-../Borne.h:
